245f575676d02ba07bf3244eb6a20ea61959af92
Scott Draves <spot@draves.org>
2018-02-01 07:39:41 -0500

    fix https://github.com/twosigma/beakerx/issues/6748 (#20)
    * fix https://github.com/twosigma/beakerx/issues/6748
    
    * bump build number

--- recipe/pre-unlink.bat
+++ recipe/pre-unlink.bat
@@ -1,8 +1,6 @@
 @echo off
 (
   REM Run BeakerX uninstall script
-  "%PREFIX%\Scripts\beakerx-install.exe" --disable
-  REM Uninstall BeakerX notebook extension
-  "%PREFIX%\Scripts\jupyter-nbextension.exe" uninstall beakerx --py --sys-prefix
+  "%PREFIX%\Scripts\beakerx.exe" "uninstall"
   REM TODO: Restore original custom CSS and assets to notebook custom directory
 ) >>"%PREFIX%\.messages.txt" 2>&1
