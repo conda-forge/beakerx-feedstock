4afd93208a04f6450bd35f6dc010ebe811019206
Scott Draves <spot@draves.org>
2018-01-31 23:55:03 -0500

    fix https://github.com/twosigma/beakerx/issues/6748

--- recipe/pre-unlink.sh
+++ recipe/pre-unlink.sh
@@ -1,7 +1,5 @@
 {
   # Run Run BeakerX uninstall script
-  "${PREFIX}/bin/beakerx-install" --disable
-  # Uninstall BeakerX notebook extension
-  "${PREFIX}/bin/jupyter-nbextension" uninstall beakerx --py --sys-prefix
+  "${PREFIX}/bin/beakerx" "uninstall"
   # TODO: Restore original custom CSS and assets to notebook custom directory
 } >> "${PREFIX}/.messages.txt" 2>&1
